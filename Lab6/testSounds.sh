2f5c89304a049751f2887c342407ae49e118e4bc
Arden Diak <ardendiak@gmail.com>
2021-12-12 15:33:03 +0100

    correct Cheby, Butters freq resp plots ~30min

--- Lab6/testSounds.sh
+++ Lab6/testSounds.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for audiofile in audioFiles/*.wav; do
-    echo "$audiofile"
-    afplay "$audiofile"
+band_pass_filters=(1 2 3 4 5 6 7 8 9 10)
+
+for t in ${band_pass_filters[@]}; do
+    echo audioFiles/band_pass_"$t".wav
+    afplay audioFiles/band_pass_"$t".wav;
     sleep 1
 done
